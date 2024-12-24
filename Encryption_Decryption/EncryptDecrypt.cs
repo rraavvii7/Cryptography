7b57f238f8f6c03400a0043d2bc363c2395b1339
rraavvii7 <rraavvii7@gmail.com>
2024-12-24 15:53:27 +0530

    First commit

---
+++ Encryption_Decryption/EncryptDecrypt.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Encryption_Decryption
+{
+	public class EncryptDecrypt
+	{
+        private readonly string KEY;
+        private const short KEYSIZE = 256;
+        private const short IVSIZE = 16;
+
+        public EncryptDecrypt(string key)
+		{
+            KEY = key;
+		}
+        public string Encrypt(string dataToEncrypt)
+        {
+            if (string.IsNullOrEmpty(KEY))
+            {
+                throw new ArgumentNullException(nameof(KEY));
+            }
+            if (string.IsNullOrEmpty(dataToEncrypt))
+            {
+                return string.Empty;
+            }
+
+            byte[] array;
+            using (Aes aes = Aes.Create())
+            {
+                aes.KeySize = KEYSIZE;
+                aes.Key = Encoding.UTF8.GetBytes(KEY);
+                aes.Mode = CipherMode.CBC;
+                aes.Padding = PaddingMode.PKCS7;
+                aes.GenerateIV();
+
+                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    memoryStream.Write(aes.IV);
+                    using (CryptoStream cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter streamWriter = new StreamWriter(cryptoStream))
+                        {
+
+                            streamWriter.Write(dataToEncrypt);
+                        }
+                        array = memoryStream.ToArray();
+                    }
+                }
+            }
+            string result = Convert.ToBase64String(array);
+            return result;
+        }
+
+        public string Decrypt(string dataToDecrypt)
+        {
+            if (string.IsNullOrEmpty(KEY))
+            {
+                throw new ArgumentNullException(nameof(KEY));
+            }
+            if (string.IsNullOrEmpty(dataToDecrypt))
+            {
+                return string.Empty;
+            }
+
+            byte[] iv = new byte[IVSIZE];
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.KeySize = KEYSIZE;
+                aes.Key = Encoding.UTF8.GetBytes(KEY);
+                aes.Mode = CipherMode.CBC;
+                aes.Padding = PaddingMode.PKCS7;
+                var buffer = Convert.FromBase64String(dataToDecrypt);
+                using (MemoryStream memoryStream = new MemoryStream(buffer))
+                {
+                    int read = memoryStream.Read(iv, 0, IVSIZE);
+                    using (CryptoStream cryptoStream = new CryptoStream(memoryStream, aes.CreateDecryptor(aes.Key, iv), CryptoStreamMode.Read))
+                    {
+                        using (StreamReader streamReader = new StreamReader(cryptoStream))
+                        {
+                            return streamReader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
---
+++ Encryption_Decryption/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Encryption_Decryption;
+
+Console.WriteLine("Hello, World!");
+
+EncryptDecrypt ed = new("+=jsdkkdlo4454GG00155sajuklmbk%@");
+
+var encryptedData = ed.Encrypt("I wish if I were extremely rich");
+Console.WriteLine(encryptedData);
+var decryptedData = ed.Decrypt(encryptedData);
+Console.WriteLine(decryptedData);
+
