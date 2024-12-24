4cca1ee276ddf7d55374f61a60e94fb382313f56
rraavvii7 <rraavvii7@gmail.com>
2024-12-24 16:11:37 +0530

    added hashing

---
+++ Encryption_Decryption/Hashing.cs
@@ -0,0 +1,32 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Encryption_Decryption
+{
+	public static class Hashing
+	{
+        public static StringBuilder CreateHash(string input)
+        {
+
+            // Convert the input string to a byte array
+            byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+
+            // Compute the hash using SHA256
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] hashBytes = sha256.ComputeHash(inputBytes);
+
+                // Convert the hash byte array to a hexadecimal string
+                StringBuilder sb = new StringBuilder();
+                foreach (byte b in hashBytes)
+                {
+                    sb.Append(b.ToString("x2")); // Converts byte to 2-digit hex
+                }
+                return sb;
+                
+            }
+        }
+        
+	}
+}
+
--- Encryption_Decryption/Program.cs
+++ Encryption_Decryption/Program.cs
@@ -1,12 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Encryption_Decryption;
 
-Console.WriteLine("Hello, World!");
 
+//symmetric encryption
 EncryptDecrypt ed = new("+=jsdkkdlo4454GG00155sajuklmbk%@");
 
 var encryptedData = ed.Encrypt("I wish if I were extremely rich");
 Console.WriteLine(encryptedData);
 var decryptedData = ed.Decrypt(encryptedData);
 Console.WriteLine(decryptedData);
+
+
+//hashing
+var hashedData1 = Hashing.CreateHash("ravi").ToString();
+var hashedData2 = Hashing.CreateHash("ravi").ToString();
+Console.WriteLine(hashedData1);
+Console.WriteLine(hashedData2);
+
+Console.WriteLine(string.Equals(hashedData1, hashedData2, StringComparison.Ordinal));
+Console.ReadLine();
+
 
