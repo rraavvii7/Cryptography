35b081c7f061435ba5da151931e1d266d6697117
rraavvii7 <rraavvii7@gmail.com>
2024-12-24 16:30:53 +0530

    added assymetric encryption RSA

---
+++ Encryption_Decryption/AssymeticEncryptDecrypt.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Encryption_Decryption
+{
+	public static class AssymeticEncryptDecrypt
+	{
+
+        public static byte[] EncryptData(byte[] data, string publicKeyBase64)
+        {
+            using (RSA rsa = RSA.Create())
+            {
+                rsa.ImportRSAPublicKey(Convert.FromBase64String(publicKeyBase64), out _);
+                return rsa.Encrypt(data, RSAEncryptionPadding.OaepSHA256);
+            }
+        }
+
+        public static byte[] DecryptData(byte[] data, string privateKeyBase64)
+        {
+            using (RSA rsa = RSA.Create())
+            {
+                rsa.ImportRSAPrivateKey(Convert.FromBase64String(privateKeyBase64), out _);
+                return rsa.Decrypt(data, RSAEncryptionPadding.OaepSHA256);
+            }
+        }
+    }
+}
+
--- Encryption_Decryption/Program.cs
+++ Encryption_Decryption/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Security.Cryptography;
+using System.Text;
 using Encryption_Decryption;
 
 
@@ -9,6 +11,28 @@ var encryptedData = ed.Encrypt("I wish if I were extremely rich");
 Console.WriteLine(encryptedData);
 var decryptedData = ed.Decrypt(encryptedData);
 Console.WriteLine(decryptedData);
+
+
+//assymetric encryption
+var originalData = "this is for assymetric encryption using public key and private key";
+using (RSA rsa = RSA.Create())
+{
+    // Export public and private keys
+    string publicKey = Convert.ToBase64String(rsa.ExportRSAPublicKey());
+    string privateKey = Convert.ToBase64String(rsa.ExportRSAPrivateKey());
+
+    Console.WriteLine("Public Key:\n" + publicKey);
+    Console.WriteLine("\nPrivate Key:\n" + privateKey);
+
+    // Encrypt the data using the public key
+    byte[] encryptedDataBytes = AssymeticEncryptDecrypt.EncryptData(Encoding.UTF8.GetBytes(originalData), publicKey);
+    Console.WriteLine("\nEncrypted Data (Base64):\n" + Convert.ToBase64String(encryptedDataBytes));
+
+    // Decrypt the data using the private key
+    byte[] decryptedDataBytes = AssymeticEncryptDecrypt.DecryptData(encryptedDataBytes, privateKey);
+    string decryptedMessage = Encoding.UTF8.GetString(decryptedDataBytes);
+    Console.WriteLine("\nDecrypted Message:\n" + decryptedMessage);
+}
 
 
 //hashing
